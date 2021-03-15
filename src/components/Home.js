d6f09766a85143ac5d04fbf41fc4cfbcb4c84762
Mikael <widell79@gmail.com>
2021-03-15 16:07:28 +0100

    feat: map questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,27 @@ import { selectQuestions } from "../features/questions/questionsSlice";
 
 export function Home() {
   const dispatch = useDispatch();
+  const questions = useSelector(selectQuestions);
+
+  function mapStateToList(questions) {
+    return {
+      questionsIds: Object.keys(questions).sort(
+        (a, b) => questions[b].timestamp - questions[a].timestamp
+      ),
+    };
+  }
+
+  const questionList = mapStateToList(questions);
+  console.log(questionList);
 
   return (
     <div>
       <h3 className="center">Questions</h3>
-
-      <ul className="dashboard-list"></ul>
+      <ul className="dashboard-list">
+        {questionList.questionsIds.map((id) => (
+          <li key={id}>{id}</li>
+        ))}
+      </ul>
     </div>
   );
 }
