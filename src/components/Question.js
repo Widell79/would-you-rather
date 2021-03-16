f1791064ab8d76ec9e24befd22ccedd0007da77a
Mikael <widell79@gmail.com>
2021-03-16 14:45:32 +0100

    refactor: Moved avatars folder into public

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,38 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
+
+import { selectUsers } from "../features/users/usersSlice";
 
 const Question = ({ question }) => {
   //   if (question.id === null) {
   //     return <p>This Question doesn't exist</p>;
   //   }
 
+  const users = useSelector(selectUsers);
+
+  function mapStateToList(users) {
+    return {
+      usersIds: Object.keys(users),
+      usersValue: Object.values(users),
+    };
+  }
+
+  const usersList = mapStateToList(users);
+  const avatar = usersList.usersValue
+    .filter((user) => {
+      return user.id === question.author;
+    })
+    .map((user) => {
+      return user.avatarURL;
+    });
+
   return (
     <div className="tweet">
-      {/* <img src="" alt={`Avatar of `} className="avatar" /> */}
+      {/* {usersList.usersValue.filter((user) => {
+        return <p>user.id !== question.author</p>;
+      })} */}
+      <img src={avatar} alt={`Avatar of `} className="avatar" />
       <div className="tweet-info">
         <div>
           <span>{question.author}</span>
